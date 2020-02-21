5a996d0a9ec86de4a19053e5eadadfeacd8a924c
oni-sbp <58467574+oni-sbp@users.noreply.github.com>
2020-02-21 11:59:20 +0200

    Update generate-docs.sh

--- scripts/generate-docs.sh
+++ scripts/generate-docs.sh
@@ -7,7 +7,6 @@ cd CodeGen-Code
 npm install
 
 cd doc-generation
-cp -r src ../docs/raml2markdown/node_modules/swagger-to-slate
 cp -r lib ../docs/raml2markdown/oas-raml-converter/
 cd ..
 
@@ -25,6 +24,10 @@ npm install
 
 cd ..
 cd ..
+cd ..
+
+cd doc-generation
+cp -r src ../docs/raml2markdown/node_modules/swagger-to-slate
 cd ..
 
 node API-Engine-cmd.js --input ./raml2markdown/src --output ./build --host $HOST --scheme $SCHEME
